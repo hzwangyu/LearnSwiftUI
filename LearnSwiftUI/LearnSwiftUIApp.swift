95ba20da8ad9894b011ef9f8456e0f745ee2c718
cswangyu <cswangyu@icloud.com>
2021-08-08 19:21:56 +0800

    Initial Commit

---
+++ LearnSwiftUI/LearnSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnSwiftUIApp.swift
+//  LearnSwiftUI
+//
+//  Created by hzwangyu on 2021/8/8.
+//
+
+import SwiftUI
+
+@main
+struct LearnSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
